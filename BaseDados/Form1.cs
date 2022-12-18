ad354622c893de30b2544e0857804a05d2c5850b
Adroaldo <hygino@msn.com>
2022-12-18 14:11:53 -0300

    Configurando o SQLite

--- BaseDados/Form1.cs
+++ BaseDados/Form1.cs
@@ -12,6 +12,9 @@ using System.IO;
 //Sql Server CE
 using System.Data.SqlServerCe;
 
+//SQLite
+using System.Data.SQLite;
+
 namespace BaseDados
 {
     public partial class Form1 : Form
@@ -23,7 +26,8 @@ namespace BaseDados
 
         private void btnConectar_Click(object sender, EventArgs e)
         {
-            string baseDados = Application.StartupPath + "\\db\\DBSQLServer.sdf";
+            #region SQL Server CE
+            /*string baseDados = Application.StartupPath + "\\db\\DBSQLServer.sdf";
             string strConnection = @"DataSource = " + baseDados + "; Password = '1234567'";
             SqlCeEngine db = new SqlCeEngine(strConnection);
 
@@ -50,7 +54,38 @@ namespace BaseDados
             finally
             {
                 conexao.Close();//fecha a conexão com o bd
+            }*/
+            #endregion
+
+            #region SQLite
+            string baseDados = Application.StartupPath + "\\db\\DBSQLite.db";
+            string strConnection = @"Data Source = " + baseDados + "; Version = 3";
+
+            if (!File.Exists(baseDados))
+            {
+                SQLiteConnection.CreateFile(baseDados);
             }
+
+            SQLiteConnection conexao = new SQLiteConnection(strConnection);
+
+            try
+            {
+                conexao.Open();
+                labelResultado.Text = "Conectado SQLite\n";
+
+
+            }
+            catch (Exception ex)
+            {
+                labelResultado.Text = "Erro ao conectar o SQLite\n" + ex;
+            }
+            finally
+            {
+                conexao.Close();//fecha a conexão com o bd
+            }
+
+
+            #endregion
         }
     }
 }
